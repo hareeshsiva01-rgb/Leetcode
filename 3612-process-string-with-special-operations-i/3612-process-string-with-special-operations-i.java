1c2b4645c32fb9d0ea56270214dca6f13717d349
Hareesh S <hareeshsiva01@gmail.com>
2026-06-16 15:14:22 +0530

    Time: 3 ms (100%), Space: 55.1 MB (64.71%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,25 +1,24 @@
 class Solution {
     public String processStr(String s) {
-    StringBuilder sb = new StringBuilder("");
-    
-    for(int i=0;i<s.length();i++){
-        if(s.charAt(i)=='*'){
-                if(sb.length() > 0){
-                sb.deleteCharAt(sb.length()-1);
+        StringBuilder sb = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
                 }
+            }
+            else if (ch == '#') {
+                sb.append(sb);
+            }
+            else if (ch == '%') {
+                sb.reverse();
+            }
+            else {
+                sb.append(ch);
+            }
         }
-        else if(s.charAt(i)=='#'){
-            sb.append(sb);
-           
-        }
-        else if(s.charAt(i)=='%'){
-            sb.reverse();
-        }
-        else{
-            sb.append(s.charAt(i));
-           
-        }
-    }
-    return sb.toString(); 
+
+        return sb.toString();
     }
 }
